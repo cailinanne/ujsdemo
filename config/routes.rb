28dff25db8ad0984e6eb969048aa0f27712f1b3a
Cailin Nelson <cailinnelson@cailin-nelsons-macbook-pro.local>
2012-02-06 16:44:05 -0500

    Add generic articles scaffold.

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Ujsdemo::Application.routes.draw do
+  resources :articles
+
   # The priority is based upon order of creation:
   # first created -> highest priority.
 
@@ -48,7 +50,7 @@ Ujsdemo::Application.routes.draw do
 
   # You can have the root of your site routed with "root"
   # just remember to delete public/index.html.
-  # root :to => 'welcome#index'
+  root :to => 'articles#index'
 
   # See how all your routes lay out with "rake routes"
 
